adbfe2ecdd1727fb1717560a5cc6c9bfc8417a01
pingyuangulang <jibin_0310@163.com>
2017-11-28 17:38:33 +0800

    create file a1.java

---
+++ a1.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+import java.util.List;
+public class Solution{
+	private int number;
+	public Solution(){}
+	public Solution(int number){
+		this.number = number;
+	}
+	public static void main(String[] args){
+	
+	}
+}
